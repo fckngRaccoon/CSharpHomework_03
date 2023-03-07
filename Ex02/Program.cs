09ba2a8ac8b87474f7e5918b6beb13387f4ac9ce
cppdev <fckngRaccoon@yandex.ru>
2023-03-07 22:05:16 +0300

    Ex02

---
+++ Ex02/Program.cs
@@ -0,0 +1,24 @@
+﻿int x1 = Coord("x", "A");
+int y1 = Coord("y", "A");
+int z1 = Coord("z", "A");
+int x2 = Coord("x", "B");
+int y2 = Coord("y", "B");
+int z2 = Coord("z", "B");
+
+int Coord(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2){
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {segmentLength}");
